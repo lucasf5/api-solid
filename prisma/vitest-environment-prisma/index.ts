1e631ac68b3086695cefb224d3a42bfffe074dcc
Lucas Ferreira Pinto <lucasfpnt@gmail.com>
2024-08-20 20:26:08 -0300

    github action

--- prisma/vitest-environment-prisma/index.ts
+++ prisma/vitest-environment-prisma/index.ts
@@ -1,33 +1,32 @@
-import { PrismaClient } from "@prisma/client";
-import { execSync } from "child_process";
-import { randomUUID } from "crypto";
-import env from "dotenv";
-import { Environment } from "vitest";
-env.config();
+import "dotenv/config";
 
-// "postgresql://postgres:postgres@localhost:5432/mydb?schema=public"
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
+import { Environment } from "vitest";
 
 const prisma = new PrismaClient();
 
-const generateDatabaseUrl = (schema: string) => {
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not set");
+    throw new Error("Please provide a DATABASE_URL environment variable.");
   }
 
   const url = new URL(process.env.DATABASE_URL);
+
   url.searchParams.set("schema", schema);
 
   return url.toString();
-};
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
     const schema = randomUUID();
-    const databaseUrl = generateDatabaseUrl(schema);
+    const databaseURL = generateDatabaseURL(schema);
 
-    process.env.DATABASE_URL = databaseUrl;
+    process.env.DATABASE_URL = databaseURL;
 
     execSync("npx prisma migrate deploy");
 
